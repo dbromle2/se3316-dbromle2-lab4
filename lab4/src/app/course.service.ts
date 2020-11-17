709fa6cb752fd2d9a9f18fe56e8312c8794391fa
dbromle2 <36002560+dbromle2@users.noreply.github.com>
2020-11-16 23:13:14 -0500

    GET commands working, front end angular is broken though

--- lab4/src/app/course.service.ts
+++ lab4/src/app/course.service.ts
@@ -3,17 +3,51 @@ import { Course } from "./course";
 import { COURSES } from "./mock-courses";
 import { Observable, of } from "rxjs";
 import { MessageService } from "./message.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
+  private coursesUrl = "/api/courses" //from lab3
+
+  /**GET all courses (subject and course name) **/
   getCourses(): Observable<Course[]> {
     //TODO: send the message _after_ fetching the courses
     this.messageService.add("CourseService: fetched courses");
-    return of(COURSES);
+    return this.http.get<Course[]>(this.coursesUrl).pipe(tap(_ => this.log("fetched courses")), catchError(this.handleError<Course[]>("getCourses",[])));
+  }
+
+  /**GET course by subject code, will 400 if not found **/
+  getCourse(subject: string): Observable<Course>{
+    const url = `${this.coursesUrl}/${subject}`;
+    return this.http.get<Course>(url).pipe(tap(_ => this.log(`fetched course codes for given subject=${subject}`)), catchError(this.handleError<Course>(`getCourse subject=${subject}`)));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`CourseService: ${message}`);
+  }
+
+  /*Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
+
---
+++ lab4/src/app/schedule.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Schedule } from "./schedule";
+import { COURSES } from "./mock-courses";
+import { Observable, of } from "rxjs";
+import { MessageService } from "./message.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduleService {
+
+  constructor(private messageService: MessageService) { }
+
+  getCourses(): Observable<Schedule[]> {
+    //TODO: send the message _after_ fetching the courses
+    this.messageService.add("ScheduleService: fetched schedules");
+    return of(COURSES);
+  }
+}
